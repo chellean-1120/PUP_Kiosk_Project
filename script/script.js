67795d0c554eef07480627a5099b969826f8c17e
rochelle-pedralba <pedralbarochellean@gmail.com>
2025-07-03 23:52:43 +0800

    add all files

---
+++ script/script.js
@@ -0,0 +1,114 @@
+var pup = {
+  "oval": "A football field that can serve as softball, soccer, and football fields for students",
+  "chapel": "An Interfaith Chapel is a sacred space designed to provide a welcoming environment for people of all faiths and beliefs to come together for prayer, reflection, and meditation, fostering spiritual growth, understanding, and respect for diversity.",
+  "west_wing": "The West Wing of the Main Building in PUP Mabini Campus.",
+  "east_wing": "The East Wing of the Main Building in PUP Mabini Campus.",
+  "north_wing": "The North Wing of the Main Building in PUP Mabini Campus.",
+  "south_wing": "The South Wing of the Main Building in PUP Mabini Campus. This is a building for computer laboratory.",
+  "dome": "It is located at the center of the Main Building in PUP Mabini Campus.",
+  "ferry_station": "The Polytechnic University of the Philippines (PUP) has a ferry station located on the north side of the Pasig River, by the Philippine Coast Guard Station. This station is part of the Pasig River Ferry system; back then, it operates from 7:00 AM to 6:30 PM and includes 13 active stations.",
+  "nutri-foodsci-bldg": "The building is situated in a vibrant educational environment and is easily accessible from various parts of the campus. It provides a conducive learning environment for students pursuing studies in Nutrition and Food Science.",
+  "grandstand": "The Grandstand is often used for various university-wide events, including sports activities, university gatherings, and other special events. It provides a large open space for students and faculty to gather for these events.",
+  "mabini_shrine": "The Mabini Shrine at the Polytechnic University of the Philippines (PUP) Main Campus is a historic site located in Santa Mesa, Manila. It is noted for being the residence of Filipino military leader Apolinario Mabini, who figured prominently in the Philippine Revolution.",
+  "freedom_park": "The park is named after Nemesio E. Prudente, a former president of PUP, and is often referred to as the Nemesio E. Prudente Freedom Park. The Freedom Park is a large open space that serves as a hub for various university-wide events, including sports activities, university gatherings, and other special events.",
+  "flagpole": "The flagpole is a symbol of national pride and is used during flag ceremonies and other important university events. It stands tall and proud, representing the university’s commitment to upholding the values and principles of the nation.",
+  "amphitheater": "The amphitheater is a semi-circular or circular space for public performances and gatherings. It is often used for various university-wide events, including cultural performances, university gatherings, and other special events. It provides a large open space for students and faculty to gather for these events.",
+  "water_tank": "The water tank is managed by the Facility Management Office. The water tank plays a vital role in the university’s water supply system, ensuring that there is a steady supply of water for the various needs of the university. This includes drinking water, sanitation, and other uses.",
+  "lab_high": "The PUP LHS was established in 1954 during the Philippine College of Commerce era. The PUP LHS curricular program focuses on Discipline, Academic Excellence, Nationalism, and Commerce. It is one of the top-performing high schools in the country, producing top-caliber students who later become prominent in their chosen field of specialization.",
+  "charlie_bldg": "The building is named after Carlos “Charlie” del Rosario, a former instructor of Political Science and History at the Philippine College of Commerce (now PUP) and a ranking member of the Movement for a Democratic Philippines (MDP) secretariat. At the present, Charlie Del Rosario Building is open to house student organizations.",
+  "student_canteen": "The canteen offers a variety of food services from private concessionaires renting food stalls. It provides take-out food services and can be reserved for events and meetings. The canteen is a hub for students to grab a meal, have a coffee, or take a break from their classes.",
+  "printing_press": "The Printing Press Building houses the University Printing Press Office, which includes the Pre-Press and Digital Printing Section. The Printing Press Building plays a vital role in the university’s operations, providing printing services for various university needs. It is situated near other significant buildings on the campus.",
+  "library": "The NALLRC is one of the major service centers of PUP and strives to meet the academic and related needs of its clientele through the provision of adequate and efficient library and information services. The library offers a variety of services including scanning services, free internet access at the PUP-CLFI E-Learning Center, and free access to thousands of e-resources available for PUP students, faculty, and staff through CHED’s Philippine e-Lib Project.",
+  "tahanan_alumni": "The Tahanan ng Alumni is a building dedicated to the alumni of PUP and serves as a venue for various alumni events and activities. One of the notable events held at the Tahanan ng Alumni is the Groundbreaking Ceremony of the PUP Tahanan ng Atleta, which is a project of the PUP Alumni Association USA (PUPAA-USA). The PUPAA-USA raised $175,000 during the first PUP/PCC Global Reunion in Las Vegas to make this dream come true for the university and the athletes.",
+  "swimming_pool": "An olympic-sized swimming pool that is capable of handling several swimming classes and student athlete activities simultaneously.",
+  "gymnasium": "The University Gymnasium serves as the training ground for the student athletes that participate in the intercollegiate sports program.",
+  "tennis_court": "The tennis court is part of the outdoor gym. It is often used for various university-wide events, including sports activities and physical education classes.",
+  "outdoor_bb_vb_court": "The basketball and volleyball court is part of the outdoor gym. It is often used for various university-wide events, including sports activities and physical education classes.",
+  "info_center": "The Information Center serves as a hub for all inquiries and information related to the university. The center provides information about the university’s academic programs, admission procedures, campus events, and other relevant information. It is often the first point of contact for students, faculty, and visitors seeking information about the university.",
+  "lagoon": "The Lagoon is the center of PUP and the canteen of the campus. It is where you would find PUPians eating and taking a break from their classes. After more than a year of renovation, the new PUP Lagoon was inaugurated on December 16, 202133. The new PUP Lagoon has new landscapes and hardscapes, and renovated stalls. The new lagoon will also serve as a park and open space for students.",
+  "obelisk": "The Obelisk, standing majestic on its base, depicts the strength of PUP as an institution of higher learning, promoting educational and moral aims which are fortified by a determined leadership with a clear vision for the Filipino youth and an efficient support system inspired by the virtues of public service.",
+  "linear_park": "The Linear Park is situated nearby to the food court, Student Canteen and the theater building, PUP Auditorium. Despite being located near the Pasig River, which sometimes gives off an unpleasant smell; the Linear Park is still one of the favorite spots for PUP students to relax due to its breezy riverside location.",
+  "pasig_river": "The Pasig River is a water body in the Philippines that connects Laguna de Bay to Manila Bay. Stretching for 25.2 kilometers, it bisects the Philippine capital of Manila and its surrounding urban area into northern and southern halves.",
+  "outside_canteen": "A place where student can have a seat and eat their lunch. It is located beside the University Canteen.",
+  "souvenir_shop": "The shop offers a wide range of products, from apparel to novelty items, all bearing the name and official logo of the university. Each item serves as a memento of the university’s spirit and pride.",
+  "hk_building": "The Human Kinetics Building is more than just a structure; it represents PUP’s commitment to promoting physical fitness and sports among its students and the community. It stands as a testament to the university’s dedication to providing quality education and facilities for its students.",
+  "nutri_diet_bldg": "The building features modern facilities, including a food laboratory for hands-on training in food preparation and meal management, and a nutrition assessment laboratory equipped with tools for dietary and nutritional assessment.",
+  "pylon": "The Pylon originally stood for the true, the good, and the beautiful. The triad of pillars may also stand for wisdom, strength, and beauty because there should be wisdom to contrive, strength to support, and beauty to adorn any great or important undertaking.",
+  "guard_house": "The guard house is typically manned by security personnel who ensure the safety and security of the students, faculty, and staff."
+}
+
+
+// Creates a div element to display the title and description of the place
+function displayBox(heading, description, imgSrc) {
+  let display = document.createElement("div");
+  document.body.appendChild(display)
+  display.id = "display";
+  display.style.position = 'absolute';
+
+  let image = document.getElementById('place-image');
+  let imageTitle = document.getElementById('image-title');
+  image.src = imgSrc;
+  image.style = "opacity: 1";
+  imageTitle.innerText = heading;
+
+  document.addEventListener('mousemove', (event) => {
+
+    let x = event.clientX;
+    let y = event.clientY;
+
+    console.log('x:' + x)
+    console.log('y:' + y)
+
+    // If it overflowed to the screen, adjust the x and y axis of the box
+    if (y >= 290) {
+
+      if (description == pup.ferry_station || description == pup.linear_park) {
+        display.style.top = event.clientY - 220 + "px";
+      } else if (description == pup.chapel) {
+        display.style.top = event.clientY - 150 + "px";
+      } else if (description == pup.pasig_river) {
+        display.style.top = event.clientY - 200 + "px";
+      } else if (description == pup.nutri_diet_bldg) {
+        display.style.top = event.clientY - 100 + "px"; 
+      } else {
+        display.style.top = event.clientY - 130 + "px";
+      }
+
+      if (x <= 1320) {
+        display.style.left = event.clientX + 20 + "px";
+      } else {
+        console.log("EVENT CLIENT:" + event.clientX)
+        display.style.left = event.clientX - 250 + "px";
+      }
+
+    } else {
+      display.style.top = event.clientY + 20 + "px";
+
+      if (x <= 1320) {
+        display.style.left = event.clientX + 20 + "px";
+      } else {
+        console.log("EVENT CLIENT:" + event.clientX)
+        display.style.left = event.clientX - 250 + "px";
+      }
+    }
+
+    display.style.position = "absolute";
+    display.innerHTML = `<div id='wrapper'><p id=heading>${heading}</p> <p id='place-description'>${description}</p></div>`
+
+  });
+}
+
+// Removes the description box
+function removeDisplay(){
+  document.getElementById('display').remove()
+
+  let image = document.getElementById('place-image');
+  image.src = "../assets/front-gate.jpg";
+  document.getElementById('image-title').innerText = "Hover over the PUP Map to display image";
+}
+
+// Changes to the next image element
+function nextImage(imgSrc) {
+  let image = document.getElementById('place-image');
+  image.src = imgSrc;
+}
